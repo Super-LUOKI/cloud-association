ec356ef81468961898df42a6e9523af9c6ae2d85
LuoKing <luoki1313@163.com>
2021-05-19 22:54:12 +0800

    完善登录页面和我的页面

--- components/Me/Me.js
+++ components/Me/Me.js
@@ -12,7 +12,9 @@ Component({
      */
     data: {
         username: '点击登录',
-        brief: '点击登录，开启你的社团之旅~'
+        brief: '点击登录，开启你的社团之旅~',
+        // 是否已经登录
+        isLogin: false
 
     },
 
@@ -26,10 +28,19 @@ Component({
             const userInfo = getUserInfo()
             if (userInfo) {
                 // 如果用户信息存在
-            } else {
-                // 如果用户信息不存在
+                this.setData({ isLogin: true })
             }
+        },
+        // 点击 头像、简介用户信息
+        handleClickInfo() {
+            // 如果没登录则提示登录，否则不做操作
+            if (!this.data.isLogin) {
+                wx.navigateTo({
+                    url: '/pages/login/index'
+                });
 
+            }
         }
+
     }
 })
